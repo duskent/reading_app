a1045ae79cfaae54859f584a051c47566cad6521
Julian Ronayi <yuliy.ronayi@azinec.com>
2017-09-11 10:35:25 +0300

    fix(merge): Conflict fix and update lock file

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,6 @@
 // Mongoose connection
-<<<<<<< HEAD
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/reading_app');
-=======
 import mongoose from 'mongoose'
 mongoose.connect('mongodb://localhost/reading_app')
->>>>>>> webpack
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
