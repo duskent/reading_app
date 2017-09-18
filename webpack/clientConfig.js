992279e5d16cf4315a66fbe67da96bdfabf49fe8
Julian Ronayi <yuliy.ronayi@azinec.com>
2017-09-18 17:06:46 +0300

    build(webpck): Add babel plugins for react

--- webpack/clientConfig.js
+++ webpack/clientConfig.js
@@ -1,5 +1,17 @@
 const {join} = require('path')
+const webpack = require('webpack')
+
 const commonPlugins = require('./commonPlugins')
+const CompressionPlugin = require('compression-webpack-plugin')
+
+commonPlugins.push(new webpack.optimize.OccurrenceOrderPlugin())
+commonPlugins.push(new CompressionPlugin({
+  asset: '[path].gz[query]',
+  algorithm: 'gzip',
+  test: /\.js$|\.html$/,
+  threshold: 10240,
+  minRatio: 0.8
+}))
 
 module.exports = {
   entry: ['babel-polyfill', './client/index.js'],
@@ -18,8 +30,14 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
-        options: {
-          presets: ['es2015', 'react', 'stage-0']
+        query: {
+          presets: ['es2015', 'react', 'stage-0'],
+          plugins: [
+            'transform-runtime',
+            'transform-react-remove-prop-types',
+            'transform-react-constant-elements',
+            'transform-react-inline-elements'
+          ]
         }
       }
     ]
